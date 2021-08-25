f6d303e4a19e912a7d99308ebc6a57809a4fcac8
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-08-25 02:31:21 +0100

    Âscenario of change for exeter

--- data/exeter.r
+++ data/exeter.r
@@ -1,27 +1,69 @@
+####
+####
+#### AIM: Use PCT data to create scenario of change where commuting cycling levels increase and car journeys decrease which can be imported
+####      into A/B Street city simulation software. This method should be fully reproducible for other pct_regions.
+####
+####
+
+####  LIBRARYS ####
 library(pct)
 library(sf)
 library(tidyverse)
 library(abstr)
-# Get zones
-devon_zones = pct::get_pct_zones(region = "devon", geography = "msoa")
-exeter_zones = devon_zones %>% filter(lad_name == "Exeter") %>% select(geo_code)
-# Get od data
-devon_od = pct::get_od(region = "devon", )
-exeter_od = devon_od %>% filter(la_1 == "Exeter" & la_2 == "Exeter")
 
-# Transform od data to abstr format
-exeter_od$Transit = apply(( exeter_od[ , c(5,6,7) ]), 1 ,sum ) # calculate transit (light_rail + train + bus)
-exeter_od$Drive = apply(( exeter_od[ , c(8,9,10,11) ]), 1 ,sum ) # calculate drive (motobike, taxi, car driver, car passenger)
+# for plotting
+# library(tmap)
+# library (mapview)
 
-exeter_od = exeter_od %>%
-  mutate(Bike = bicycle) %>%
-  mutate(All = all) %>%
-  mutate(Walk = foot) %>%
-  select(geo_code1,geo_code2,All,Bike,Transit, Drive, Walk)
+####    READ DATA ####
+devon_zones = pct::get_pct_zones(region = "devon", geography = "msoa") # get zone data
+exeter_zones = devon_zones %>% filter(lad_name == "Exeter") %>% select(geo_code) # filter for exeter
 
-# Generate buildings
+exeter_commute_od = pct::get_pct_lines(region = "devon", geography = "msoa") %>% # get commute od data
+  filter(lad_name1 == "Exeter" &
+           lad_name2 == "Exeter") # filter for exeter
+
+
+####    CLEAN DATA , CALCULATE EUCLIDEAN DISTANCE  & GENERATE SCENARIOS OF CHANGE ####
+exeter_commute_od = exeter_commute_od %>%
+  mutate(cycle_base = bicycle) %>%
+  mutate(walk_base = foot) %>%
+  mutate(transit_base = bus + train_tube) %>% # bunch of renaming -_-
+  mutate(drive_base = car_driver + car_passenger + motorbike + taxi_other) %>%
+  mutate(all_base = all) %>%
+  mutate(euclidean_distance = as.numeric(sf::st_length(exeter_commute_od))) %>%
+  mutate(
+    # create new columns
+    pcycle_godutch_uptake = pct::uptake_pct_godutch_2020(distance = rf_dist_km, gradient = rf_avslope_perc),
+    cycle_godutch_additional = pcycle_godutch_uptake * drive_base,
+    cycle_godutch = cycle_base + cycle_godutch_additional,
+    pcycle_godutch = cycle_godutch / all_base,
+    drive__godutch = drive_base - cycle_godutch_additional,
+    across(c(drive__godutch, cycle_godutch), round, 0),
+    all_go_dutch = drive__godutch + cycle_godutch + transit_base + walk_base
+  ) %>%
+  select(
+    # select variables for new df
+    geo_code1,
+    geo_code2,
+    cycle_base,
+    drive_base,
+    walk_base,
+    transit_base,
+    all_base,
+    all_go_dutch,
+    drive__godutch,
+    cycle_godutch,
+    cycle_godutch_additional,
+    pcycle_godutch
+  )
+
+identical(exeter_commute_od$all_base, exeter_commute_od$all_go_dutch) # sanity check: make sure total remains the same (not a dynamic model where population change is factored in)
+
+####    DOWNLOAD OSM BUILDING DATA ####
 osm_polygons = osmextract::oe_read(
   "https://download.geofabrik.de/europe/great-britain/england/devon-latest.osm.pbf",
+  # download osm buildings for region using geofabrik
   layer = "multipolygons"
 )
 
@@ -88,7 +130,30 @@ exeter_osm_buildings_sample = exeter_osm_buildings_all_joined %>%
 exeter_osm_buildings_tbl = exeter_osm_buildings_all %>%
   dplyr::filter(osm_way_id %in% exeter_osm_buildings_sample$osm_way_id)
 
-# abstr
+
+####  LOGIC GATE ####
+# Logic gate for go_dutch scenario of change, where cycling levels increase to a proportion reflecting the Netherlands.
+#Switch to FALSE if you want census commuting OD
+go_dutch = TRUE
+if (go_dutch == TRUE) {
+  exeter_od = exeter_commute_od %>%
+    mutate(All = all_go_dutch) %>%
+    mutate(Bike = cycle_godutch) %>%
+    mutate(Transit = transit_base) %>%
+    mutate(Drive = drive_base) %>%
+    mutate(Walk = walk_base) %>%
+    select(geo_code1, geo_code2, All, Bike, Transit, Drive, Walk,geometry)
+} else {
+  exeter_od = exeter_commute_od %>%
+    mutate(All = all_base) %>%
+    mutate(Bike = cycle_base) %>%
+    mutate(Drive = drive_base) %>%
+    mutate(Transit = transit_base) %>%
+    mutate(Walk = walk_base) %>%
+    select(geo_code1, geo_code2, All, Bike, Transit, Drive, Walk, geometry)
+}
+
+####  GENERATE A/B STREET SCENARIO ####
 output_sf = ab_scenario(
   od = exeter_od,
   zones = exeter_zones,
@@ -98,15 +163,19 @@ output_sf = ab_scenario(
   pop_var = 3,
   time_fun = ab_time_normal,
   output = "sf",
-  modes = c("All", "Walk", "Bike", "Drive", "Transit")
+  modes = c("Walk", "Bike", "Drive", "Transit")
 )
 
-# tmap::tmap_mode("view")
-# tm_basemap()
+# make map using tmap
 # tm_shape(output_sf) + tmap::tm_lines(col = "mode", lwd = .8, lwd.legeld.col = "black") +
 #   tm_shape(exeter_zones) + tmap::tm_borders(lwd = 1.2, col = "gray") +
-#   tm_text("geo_code", size = 0.6) +
+#   tm_text("geo_code", size = 0.6, col = "black") +
 #   tm_style("cobalt")
+
+#### SAVE JSON FILE ####
 output_json = ab_json(output_sf, time_fun = ab_time_normal, scenario_name = "Exeter Example")
 ab_save(output_json, f = "../../Desktop/exeter.json")
 
+#### COMMANDS FOR AB STREET
+
+# $ cargo run --bin import_traffic -- --map=data/system/us/seattle/maps/montlake.bin --input=/C:Users/natha/OneDrive/Desktop/exeter.json
