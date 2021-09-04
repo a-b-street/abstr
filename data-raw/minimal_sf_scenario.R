b76698eff1259dc647542bff87241d3a7ae37599
Robin Lovelace <rob00x@gmail.com>
2021-09-05 00:55:57 +0100

    Progress on test code for #23

--- data-raw/minimal_sf_scenario.R
+++ data-raw/minimal_sf_scenario.R
@@ -1,22 +1,29 @@
 # Aim: generate a minimal dataset corresponding to a minimal example scenario
+
 json = jsonlite::read_json("inst/extdata/minimal_scenario2.json")
-json_df = jsonlite::read_json("inst/extdata/minimal_scenario.json", simplifyVector = TRUE)
+json_df = jsonlite::read_json("inst/extdata/minimal_scenario2.json", simplifyVector = TRUE)
 # Try importing the data into abstreet
 json_df$people$trips[[1]]
-data.table::rbindlist(json_df$people$trips)
 trip_data = dplyr::bind_rows(json_df$people$trips, .id = "id")
+trip_data
+# unhelpful flat format
+# trip_data_unnested = tidyr::unnest(trip_data, cols = c(origin, destination))
+names(trip_data)
 
 # code below could be generalised to a json_to_sf() function
-linestrings = od::odc_to_sf(cbind(
+linestrings = od::odc_to_sfc(cbind(
   trip_data$origin$Position$longitude,
   trip_data$origin$Position$latitude,
   trip_data$destination$Position$longitude,
   trip_data$destination$Position$latitude
 ))
-mapview::mapview(linestrings)
-od::odc_to_sf()
-sf_data =
-sf_linestring = sf::st_sf(
+# mapview::mapview(linestrings)
 
+
+sf_data = subset(trip_data, select = -c(origin, destination))
+sf_linestring = sf::st_sf(
+  sf_data,
+  geometry =linestrings
 )
 
+plot(sf_linestring)
