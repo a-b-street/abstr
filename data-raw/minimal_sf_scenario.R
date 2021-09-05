be0078f31cf0f535918ac9bde93bc8f5764e607c
Robin Lovelace <rob00x@gmail.com>
2021-09-05 01:36:17 +0100

    Update test code and draft vignette

--- data-raw/minimal_sf_scenario.R
+++ data-raw/minimal_sf_scenario.R
@@ -1,6 +1,7 @@
 # Aim: generate a minimal dataset corresponding to a minimal example scenario
 
 json = jsonlite::read_json("inst/extdata/minimal_scenario2.json")
+json$scenario_name
 json_df = jsonlite::read_json("inst/extdata/minimal_scenario2.json", simplifyVector = TRUE)
 # Try importing the data into abstreet
 json_df$people$trips[[1]]
@@ -23,7 +24,21 @@ linestrings = od::odc_to_sfc(cbind(
 sf_data = subset(trip_data, select = -c(origin, destination))
 sf_linestring = sf::st_sf(
   sf_data,
-  geometry =linestrings
+  geometry = linestrings
 )
 
 plot(sf_linestring)
+
+json2 = ab_json(desire_lines_out = sf_linestring, scenario_name = "minimal2")
+identical(json, json2)
+waldo::compare(json, json2)
+
+ab_save(json2, "test.json")
+waldo::compare(readLines("test.json"), readLines("inst/extdata/minimal_scenario2.json"))
+
+json3 = jsonlite::toJSON(json_df)
+unclass(json3)
+class(.Last.value) # character
+json4 = jsonlite::serializeJSON(x = json_df)
+class(json4)
+unclass(json4)
