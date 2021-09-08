646a5a619f58a4ba3b9fefc0d2cba97997b835b6
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-08 23:59:37 +0100

    update data documentation

--- R/hackathon_data.R
+++
@@ -1,6 +0,0 @@
-#' Datasets for the DECARBON8 hackathon containing OD activity in Sao Paulo, Brazil.
-#'
-#' These datasets represent data from the case study city of Leeds.
-#' @name sao_paulo_activity_df_2
-#' @aliases sao_paulo_activity_df_20
-NULL
---
+++ R/hackathon_data_big.R
@@ -0,0 +1,12 @@
+#' Big OD dataset of Sao Paulo Brazil
+#'
+#' Each row contains OD data for trips made in the Sao Paulo area of Brazil for 53 people
+#' This example table is used for the DECARBON8 hackathon
+#'
+#' See the code used to create this data in "data-raw/sao-paulo-activity-data.R"
+#'
+#' @format A data frame with columns:
+#' \describe{
+#' \item{ZONA}{Trip origin zone ID.}
+#' }
+"sao_paulo_activity_df_20"
