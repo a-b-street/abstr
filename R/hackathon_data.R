3c052b0ac5ab0fa4a756ef4906ea102d68f2a892
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-08 23:36:18 +0100

    Create hackathon_data.R

---
+++ R/hackathon_data.R
@@ -0,0 +1,6 @@
+#' Datasets for the DECARBON8 hackathon containing OD activity in Sao Paulo, Brazil.
+#'
+#' These datasets represent data from the case study city of Leeds.
+#' @name sao_paulo_activity_df_2
+#' @aliases sao_paulo_activity_df_20
+NULL
