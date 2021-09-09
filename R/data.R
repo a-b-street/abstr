81e6fcac594df6db3993829618d20b76ccdb8cb8
Lucas Dias <lucasccdias0@gmail.com>
2021-09-09 17:30:46 -0300

    .Rd files issue

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Datasets from Leeds and Sao Paulo
+#' Datasets from Leeds
 #'
-#' These datasets represent OD data from Leeds and Sao Paulo.
+#' These datasets represent data from the case study city of Leeds.
 #' @name leeds_site_area
-#' @aliases leeds_houses leeds_buildings leeds_desire_lines leeds_zones leeds_od sao_paulo_activity_df_20 sao_paulo_activity_df_2
+#' @aliases leeds_houses leeds_buildings leeds_desire_lines leeds_zones leeds_od
 NULL
